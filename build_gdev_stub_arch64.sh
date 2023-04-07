f4f9ccfee86064c93828a6f1e180cb4c8a1efc6f
hi@abertschi.ch <hi@abertschi.ch>
2023-04-07 11:01:54 +0200

    build: remove mod_stub, use mod only

--- build_gdev_stub_arch64.sh
+++ build_gdev_stub_arch64.sh
@@ -3,10 +3,9 @@ set -euo pipefail
 
 CUR_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $CUR_DIR/../../../scripts/env-aarch64.sh
-MOD=$CUR_DIR/mod_stub/gdev
+MOD=$CUR_DIR/mod/gdev
 
 cd $MOD
-# make clean
 make all
 make headers
 ls -al | grep gdev_stub
